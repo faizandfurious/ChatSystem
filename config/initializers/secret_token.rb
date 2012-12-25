4ed555093be16094abd1f1f26a234ad2fa0d603d
Faiz <faizamodo@gmail.com>
2012-12-25 09:59:34 -0800

    First commit. Contains Bootstrap and initial home screen

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ChatSystem::Application.config.secret_token = '989a2c59c3c4a44e59947235819558c5ce100c138318eff9ab6d694b2c02e31d4e8e50bb75f96e104dc730ed435fb25ec46ea16ebcdc5ac94b7f2fe9b268ea94'
