6a1e71de9c0f839b264a089df50ad3bd358a0c9c
Faiz <faizamodo@gmail.com>
2013-01-19 12:52:45 -0500

    Added helper method to format date into a readable string. Doesn't change the actual value

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,12 +1,14 @@
 class Patient < ActiveRecord::Base
-  attr_accessible :address, :dob, :first_name, :last_name, :phone
+  attr_accessible :address, :dob, :first_name, :last_name, :phone, :identifier
 
 	#Relationships
 	has_many :doctors, :through => :patient_doctors
 
 	#Validations
+
 	validates_uniqueness_of :phone
 	validates_length_of :phone, :minimum => 10, :maximum => 11
+
 	before_save :phone
 
 	#Scopes
@@ -23,5 +25,9 @@ class Patient < ActiveRecord::Base
 			num = '%s(%s) %s-%s' % [ num[0], num[1,3], num[4,3], num[7,4] ]
 		end
 		self[:phone] = num
+	end
+
+	def formatted_dob
+		return dob.strftime("%B %e, %Y")
 	end
 end
